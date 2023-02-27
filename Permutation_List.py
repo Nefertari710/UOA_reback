9af44aebe9b8bbf903b2c1a205bb23274f0b2962
Nefertari710 <65151945+Nefertari710@users.noreply.github.com>
2023-02-28 11:16:30 +1300

    Update Permutation_List.py

--- Permutation_List.py
+++ Permutation_List.py
@@ -1,6 +1,6 @@
-#Permutation
+# Permutation
 
-## INIT
+# INIT
 origin = eval(input("please input a number:"))
 origin_list = [i for i in range(origin + 2)]
 pai_list = [i for i in range(origin + 2)]
@@ -15,19 +15,23 @@ pai_list[0] = origin + 1
 A_list = [i for i in range(2, origin + 1)]
 # Represent Active digit : If a digit "i" comes to boundary it will be cleaned in A_list
 
-## MAIN
+# MAIN
 Flag_Done = False
 sum = 0
 while not Flag_Done:
     sum += 1
-    print(pai_list[1:origin + 1])           # Print the permutation
-    if A_list:          # judge A_list is empty or not?
-        m = max(A_list)             # Find the maximum position in the A_list（Active position）
-        j = pai_ni_list[m]          # 3
-        pai_list[j] = pai_list[j + direction_list[m]]
-        pai_list[j + direction_list[m]] = m
-        pai_ni_list[m] = pai_ni_list[m] + direction_list[m]
-        pai_ni_list[pai_list[j]] = j
+    print(pai_list[1:origin + 1])               # Print the permutation
+    if A_list:                                  # judge A_list is empty or not?
+        m = max(A_list)                         # Find the maximum position in the A_list（Active position） eg: m = 3
+        j = pai_ni_list[m]                      # j is the maximum moving number    eg: j = 3
+
+        # Swap the data
+        pai_list[j] = pai_list[j + direction_list[m]]        # Move the maximum movable number in its direction (+1/-1)
+        pai_list[j + direction_list[m]] = m                  # Assign a value to the moved position eg: m = 3
+
+        pai_ni_list[m] = pai_ni_list[m] + direction_list[m]  # pai_ni_list represent how many step can m move
+        pai_ni_list[pai_list[j]] = j                         #
+
         if m < pai_list[j + 2 * direction_list[m]]:
             direction_list[m] = -direction_list[m]
             A_list.remove(m)
