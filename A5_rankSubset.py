7627a68691b606a66a21263a4df0839ef64d18b2
Nefertari710 <65151945+Nefertari710@users.noreply.github.com>
2023-03-12 12:13:50 +0800

    Update A5_rankSubset.py
    update and clean the mistake

--- A5_rankSubset.py
+++ A5_rankSubset.py
@@ -1,10 +1,8 @@
 from scipy.special import comb                          # import Permutation and combination
-
 R = 0
 origin_list = input("PLZ input a list:").split(" ")     # input target list e.g.[3,4,7]
-k = len(origin_list)
 
 # Use this main function can compute the Rank(list[])
-for i in range(1, k):
-    R += R + int(comb(eval(origin_list[i]) - 1, i))
+for i in range(0, len(origin_list)):
+    R += int(comb(eval(origin_list[i]) - 1, i + 1))
 print("Rank(" + str(origin_list) + ")=" + str(R))       # If you want to verify please use A4_subsetList
