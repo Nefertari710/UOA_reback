bc0670c11e0264df6b28340a595daefc9a24c737
Nefertari710 <65151945+Nefertari710@users.noreply.github.com>
2023-03-12 12:02:20 +0800

    Update A5_rankSubset.py
    Range(1:k)

--- A5_rankSubset.py
+++ A5_rankSubset.py
@@ -5,6 +5,6 @@ k = eval(input("PLZ input a k"))                        # input k
 origin_list = input("PLZ input a list:").split(" ")     # input target list e.g.[3,4,7]
 
 # Use this main function can compute the Rank(list[])
-for i in range(k):
+for i in range(1, k):
     R += R + int(comb(eval(origin_list[i]) - 1, i))
 print("Rank(" + str(origin_list) + ")=" + str(R))       # If you want to verify please use A4_subsetList
